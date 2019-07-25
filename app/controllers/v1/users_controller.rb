a9fe64df0b71e4101273cd965d7703474fd326ec
AustinBH <harlowaustin2@gmail.com>
2019-07-25 09:52:00 -0700

    added user id to index page

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -2,7 +2,7 @@ class V1::UsersController < ApplicationController
 
     def index
         users = User.all
-        render json: users, only: :username
+        render json: users, except: [:created_at, :updated_at]
     end
 
     def create
