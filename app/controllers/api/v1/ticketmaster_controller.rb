944be2a2c97269fef0d748ba70c88d6a8e1760c1
AustinBH <harlowaustin2@gmail.com>
2019-07-26 15:50:33 -0700

    updated route to display error when no events are returned

--- app/controllers/api/v1/ticketmaster_controller.rb
+++ app/controllers/api/v1/ticketmaster_controller.rb
@@ -5,8 +5,11 @@ class Api::V1::TicketmasterController < ApplicationController
         events = "https://app.ticketmaster.com/discovery/v2/events?apikey=#{ENV["TICKETMASTER_API_KEY"]}&keyword=#{params[:artist_name]}&locale=*&size=50&classificationName=music"
         response = RestClient.get(events)
         json = JSON.parse(response.body)
-        render json: json["_embedded"]["events"], only: ["name", "url", "images"]
-        # render json: json["_embedded"]["events"], only: ["name", :url, :images[0], :dates]
+        if json["_embedded"]
+            render json: json["_embedded"]["events"], only: ["name", "url", "images"]
+        else 
+            render json: {message: "there are no events for #{params[:artist_name]}"}
+        end
     end
 
     # def search_by_city
