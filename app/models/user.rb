210b30d630be64a29d6b149c4852f0680cc5dfdd
AustinBH <harlowaustin2@gmail.com>
2019-07-30 11:56:28 -0700

    added username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     has_many :songs, through: :liked_songs
     has_many :artists, through: :songs
 
+    validates :username, uniqueness: true
+
     # has_secure_password
 end
